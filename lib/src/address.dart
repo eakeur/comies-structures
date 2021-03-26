030b2043186a35acb43d0eb9bc7057fbf472015a
Igor Reis <igor.reisleandro@gmail.com>
2021-03-26 15:57:49 -0300

    lll

--- lib/src/address.dart
+++ lib/src/address.dart
@@ -1,8 +1,10 @@
 import 'package:comies_structures/comies_structures.dart';
 import 'package:comies_structures/src/costumer.dart';
+import 'package:comies_structures/src/structure.dart';
 
-class Address {
+class Address extends Structure<Address> {
   int id;
+  int costumerId;
   String cep;
   String number;
   String district;
@@ -14,4 +16,36 @@ class Address {
   String country;
   Costumer costumer;
   List<Order> orders;
+
+  @override
+  void fromMap(Map<String, dynamic> addr){
+    id = addr['id'];
+    costumerId = addr['costumerId'];
+    cep = addr['cep'];
+    street = addr['street'];
+    number = addr['number'];
+    district = addr['district'];
+    complement = addr['complement'];
+    reference = addr['reference'];
+    city = addr['city'];
+    state = addr['state'];
+    country = addr['country'];
+  }
+
+  @override
+  Map<String, dynamic> toMap(){
+    return {
+      'id': id,
+      'cep': cep,
+      'street': street,
+      'complement': complement,
+      'reference': reference,
+      'district': district,
+      'number': number,
+      'city': city,
+      'state': state,
+      'costumerId': costumerId,
+      'country': country
+    };
+  }
 }
--- lib/src/product.dart
+++ lib/src/product.dart
@@ -1,18 +1,47 @@
 import 'package:comies_structures/comies_structures.dart';
 import 'package:comies_structures/src/enum.dart';
 import 'package:comies_structures/src/order.dart';
+import 'package:comies_structures/src/structure.dart';
 
-class Product {
+class Product extends Structure<Product> {
   int id;
+  int partnerId;
   String name;
   String code;
   double min; 
   Unity unity;
   double price;
-  int partnerId;
   bool active;
   
   Partner partner;
   List<Order> orders;
+
+
+  @override
+  Map<String, dynamic> toMap(){
+    return {
+      'id': id,
+      'parnerId': partnerId,
+      'name': name,
+      'code': code,
+      'min': min,
+      'active': active,
+      'price': price,
+      'unity': unity.index,
+      'partner': partner
+    };
+  }
+
+  @override
+  void fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    name = map['name'];
+    partnerId = map['partnerId'];
+    code = map['code'];
+    min = map['min'];
+    active = map['active'];
+    price = map['price'];
+    unity = Unity.values[map['unity']];
+  }
   
 }

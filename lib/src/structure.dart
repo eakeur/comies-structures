219c389424df6c2c383141fa167b2c765fe1cd97
Igor Reis <igor.reisleandro@gmail.com>
2021-03-30 22:04:24 -0300

    hswodq

--- lib/src/structure.dart
+++ lib/src/structure.dart
@@ -1,6 +1,14 @@
+import 'dart:convert';
+
 abstract class Structure<T>{
 
   T fromMap(Map<String, dynamic> map);
+  T fromJSON(String json){
+    return fromMap(jsonDecode(json));
+  }
   Map<String, dynamic> toMap();
+
+  String get json => jsonEncode(toMap());
+
 
 }
